3a5aec667c7471d4d013362f87454311aa86c8eb
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-08 13:40:10 +0200

    Added model - Workout

---
+++ MyRun.Domain/Entities/Workout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Domain.Entities
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        public string Distance { get; set; } = default!;
+        public string? Result { get; set; }
+        public string? AveragePace { get; set; }
+        public string? MaxPace { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Calories { get; set; }
+        public string? MapPhoto { get; set; }
+        public string? Note { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
