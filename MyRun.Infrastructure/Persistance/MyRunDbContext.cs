197c356e4c05965b1b26c7f97ba17aec8139e29a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-09 11:38:25 +0200

    Created MyRunDbContext

---
+++ MyRun.Infrastructure/Persistance/MyRunDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRun.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Infrastructure.Persistance
+{
+    public class MyRunDbContext : DbContext
+    {
+        public DbSet<Race> Races { get; set; }
+        public DbSet<RunnerProfile> RunnerProfiles { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+    }
+}
