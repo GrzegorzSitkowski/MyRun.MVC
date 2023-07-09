63064578af7bad5b2b8da3a95f50d9aca1c04c84
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-09 13:19:08 +0200

    Created RaceSeeder

---
+++ MyRun.Infrastructure/Seeders/RaceSeeder.cs
@@ -0,0 +1,45 @@
+﻿using MyRun.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Infrastructure.Seeders
+{
+    public class RaceSeeder
+    {
+        private readonly MyRunDbContext _dbContext;
+
+        public RaceSeeder(MyRunDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Seed()
+        {
+            if(await _dbContext.Database.CanConnectAsync())
+            {
+                if(!_dbContext.Races.Any())
+                {
+                    var marathonPoznan = new Domain.Entities.Race()
+                    {
+                        Name = "Marathon Poznan",
+                        Country = "Poland",
+                        City = "Poznan",
+                        Number = "5175",
+                        Place = "1827",
+                        Distance = "42.197",
+                        Result = "03:47:56",
+                        AveragePace = "05:16",
+                        Date = DateTime.Now,
+                        Note = "PB",
+                        CreatedAt = DateTime.Now
+                    };
+                    _dbContext.Races.Add(marathonPoznan);
+                    await _dbContext.SaveChangesAsync();
+                }
+            }
+        }
+    }
+}
