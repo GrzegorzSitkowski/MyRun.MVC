11fbecb72cf3135aef33172b1aee6fe40c72bb14
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-10 23:01:26 +0200

    Created RaceController

---
+++ MyRun.MVC/Controllers/RaceController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyRun.MVC.Controllers
+{
+    public class RaceController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(Domain.Entities.Race race)
+        {
+            return View();
+        }
+    }
+}
