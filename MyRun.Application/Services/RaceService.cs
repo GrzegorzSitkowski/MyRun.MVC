be1310d1a7693e981445ad7a3dd97a0866f817a2
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-10 23:11:40 +0200

    Created RaceService

---
+++ MyRun.Application/Services/RaceService.cs
@@ -0,0 +1,24 @@
+﻿using MyRun.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.Services
+{
+    public  class RaceService
+    {
+        private readonly IRaceRepository _raceRepository;
+
+        public RaceService(IRaceRepository raceRepository)
+        {
+            _raceRepository = raceRepository;
+        }
+
+        public async Task Create(Domain.Entities.Race race)
+        {
+            await _raceRepository.Create(race);
+        }
+    }
+}
