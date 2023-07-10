8c8ef12fbf40bf77375fa43a354ce40405306bb8
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-10 23:05:04 +0200

    Created IRaceRepository

---
+++ MyRun.Domain/Interfaces/IRaceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Domain.Interfaces
+{
+    public interface IRaceRepository
+    {
+        Task Create(Entities.Race race);
+    }
+}
