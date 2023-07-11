8c742904dc5cbc8a4f792795950ce457134b9fae
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-11 14:54:33 +0200

    Added MyRunMappingProfile

---
+++ MyRun.Application/Mappings/MyRunMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MyRun.Application.Race;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.Mappings
+{
+    public class MyRunMappingProfile : Profile
+    {
+        public MyRunMappingProfile()
+        {
+            CreateMap<RaceDto, Domain.Entities.Race>();
+        }
+    }
+}
