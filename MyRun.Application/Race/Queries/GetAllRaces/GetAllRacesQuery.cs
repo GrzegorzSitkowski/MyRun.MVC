4c5001f89a4d379ae91256b90ba598a65fa606a3
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-15 17:40:19 +0200

    Added GetAllRacesQuery

---
+++ MyRun.Application/Race/Queries/GetAllRaces/GetAllRacesQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.Race.Queries.GetAllRaces
+{
+    public class GetAllRacesQuery : IRequest<IEnumerable<RaceDto>>
+    {
+    }
+}
