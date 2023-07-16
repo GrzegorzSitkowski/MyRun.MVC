a3da9ac8a8e2fc2f880aa11793de1d595c27c622
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-16 12:22:10 +0200

    Created GetRaceDetailsQueryHandler

---
+++ MyRun.Application/Race/Queries/GetRaceDetails/GetRaceDetailsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MyRun.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.Race.Queries.GetRaceDetails
+{
+    public class GetRaceDetailsQueryHandler : IRequestHandler<GetRaceDetailsQuery, RaceDto>
+    {
+        private readonly IRaceRepository _raceRepository;
+
+        public GetRaceDetailsQueryHandler(IRaceRepository raceRepository)
+        {
+            _raceRepository = raceRepository;
+        }
+
+        public async Task<RaceDto> Handle(GetRaceDetailsQuery request, CancellationToken cancellationToken)
+        {
+            _raceRepository.GetById(request._idRace);
+        }
+    }
+}
