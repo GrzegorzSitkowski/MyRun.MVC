3e3feed245706ef2ae1db641cd236e8dac264685
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-19 18:35:58 +0200

    Created EditRaceCommandHandler

---
+++ MyRun.Application/Race/Commands/EditRace/EditRaceCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using MyRun.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.Race.Commands.EditRace
+{
+    public class EditRaceCommandHandler : IRequestHandler<EditRaceCommand>
+    {
+        private readonly IRaceRepository _raceRepository;
+
+        public EditRaceCommandHandler(IRaceRepository raceRepository)
+        {
+            _raceRepository = raceRepository;
+        }
+
+        public async Task<Unit> Handle(EditRaceCommand request, CancellationToken cancellationToken)
+        {
+            var race = await _raceRepository.GetById(request.Id!);
+
+            race.Name = request.Name;
+            race.Country = request.Country;
+            race.City = request.City;
+            race.Number = request.Number;
+            race.Place = request.Place;
+            race.Distance = request.Distance;
+            race.Result = request.Result;
+            race.AveragePace = request.AveragePace;
+            race.Date = request.Date;
+            race.Note = request.Note;
+        }
+    }
+}
