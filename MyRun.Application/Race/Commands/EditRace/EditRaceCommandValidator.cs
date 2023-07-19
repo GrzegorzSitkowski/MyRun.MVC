a0d646a3ca8f46774b2c9efe994c8d7540ac3582
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-19 18:40:49 +0200

    Implemented EditRaceCommandValidator

---
+++ MyRun.Application/Race/Commands/EditRace/EditRaceCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.Race.Commands.EditRace
+{
+    public class EditRaceCommandValidator : AbstractValidator<EditRaceCommand>
+    {
+        public EditRaceCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().MinimumLength(2).MaximumLength(30);
+            RuleFor(c => c.Country).MaximumLength(20);
+            RuleFor(c => c.City).MaximumLength(20);
+            RuleFor(c => c.Number).MaximumLength(10);
+            RuleFor(c => c.Place).MaximumLength(10);
+            RuleFor(c => c.Distance).NotEmpty().MaximumLength(20);
+            RuleFor(c => c.AveragePace).MaximumLength(5);
+            RuleFor(c => c.Result).MaximumLength(15);
+            RuleFor(c => c.Note).MaximumLength(30);
+        }
+    }
+}
