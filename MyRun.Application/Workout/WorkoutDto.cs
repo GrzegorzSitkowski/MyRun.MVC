5c5a9ae991a1a07ca04406b2a23edb99411c7d19
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-23 13:10:51 +0200

    Created WorkoutDto

---
+++ MyRun.Application/Workout/WorkoutDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.Workout
+{
+    public class WorkoutDto
+    {
+        public int Id { get; set; }
+        public string Distance { get; set; } = default!;
+        public string? Result { get; set; }
+        public string? AveragePace { get; set; }
+        public string? MaxPace { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Calories { get; set; }
+        public string? MapPhoto { get; set; }
+        public string? Note { get; set; }
+    }
+}
