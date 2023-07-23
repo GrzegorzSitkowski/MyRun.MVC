4678286a877e96847f555b0d2f6be586c2d1a37c
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-23 13:23:36 +0200

    Created WorkoutController

---
+++ MyRun.MVC/Controllers/WorkoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyRun.MVC.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
