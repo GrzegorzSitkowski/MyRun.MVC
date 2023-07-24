45e399dcba4c00cb040ee94136a44549aed5dfc7
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-24 18:45:46 +0200

    Implemented GetWorkoutDetailsQuery

---
+++ MyRun.Application/Workout/Queries/GetWorkoutDetails/GetWorkoutDetailsQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.Workout.Queries.GetWorkoutDetails
+{
+    public class GetWorkoutDetailsQuery : IRequest<WorkoutDto>
+    {
+        public int _idWorkout { get; set; }
+
+        public GetWorkoutDetailsQuery(int idWorkout)
+        {
+            _idWorkout = idWorkout;
+        }
+    }
+}
