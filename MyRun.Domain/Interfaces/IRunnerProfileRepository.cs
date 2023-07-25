b592f8284eb33256a007f9801b92763b7fd507a5
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-25 19:22:09 +0200

    Created IRunnerProfile. Added method GetProfile

---
+++ MyRun.Domain/Interfaces/IRunnerProfileRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Domain.Interfaces
+{
+    public interface IRunnerProfileRepository
+    {
+        Task<Domain.Entities.RunnerProfile> GetProfile();
+    }
+}
