69db1b5264de4316b14cb83bb904698fdd8c9148
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-25 19:44:13 +0200

    Created RunnerProfileController

---
+++ MyRun.MVC/Controllers/RunnerProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyRun.MVC.Controllers
+{
+    public class RunnerProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
