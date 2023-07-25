f487f061a3f3b958e69a28d11cbed4b58207ab5c
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-25 18:44:59 +0200

    Implemented DeleteWorkoutCommandHandler

---
+++ MyRun.Application/Workout/Commands/DeleteWorkout/DeleteWorkoutCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MyRun.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.Workout.Commands.DeleteWorkout
+{
+    public class DeleteWorkoutCommandHandler : IRequestHandler<DeleteWorkoutCommand>
+    {
+        private readonly IWorkoutRepository _workoutRepository;
+
+        public DeleteWorkoutCommandHandler(IWorkoutRepository workoutRepository)
+        {
+            _workoutRepository = workoutRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteWorkoutCommand request, CancellationToken cancellationToken)
+        {
+            await _workoutRepository.Delete(request.Id);
+
+            return Unit.Value;
+        }
+    }
+}
