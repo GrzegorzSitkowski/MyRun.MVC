fade0d35d88d07dd10729a5446a6e644645905f5
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-07-25 19:25:21 +0200

    Created RunnerProfileRepository. Implemented method GetProfile.

---
+++ MyRun.Infrastructure/Repositories/RunnerProfileRepository.cs
@@ -0,0 +1,24 @@
+﻿using MyRun.Domain.Entities;
+using MyRun.Domain.Interfaces;
+using MyRun.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Infrastructure.Repositories
+{	
+    public class RunnerProfileRepository : IRunnerProfileRepository
+    {
+        private readonly MyRunDbContext _dbContext;
+
+        public RunnerProfileRepository(MyRunDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<RunnerProfile> GetProfile()
+            => await _dbContext.RunnerProfiles.FindAsync();
+            
+    }
+}
