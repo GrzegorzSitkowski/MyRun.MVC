fcfd0ad3c5bc3e542ffbbef9bdfa758e9dabd965
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-08-01 18:53:41 +0200

    Added CreateRunnerProfileValidator

---
+++ MyRun.Application/RunnerProfile/Commands/CreateRunnerProfile/CreateRunnerProfileValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.RunnerProfile.Commands.CreateRunnerProfile
+{
+    public  class CreateRunnerProfileValidator : AbstractValidator<CreateRunnerProfileCommand>
+    {
+        public CreateRunnerProfileValidator()
+        {
+            RuleFor(c => c.FullName).NotEmpty().MaximumLength(30);
+            RuleFor(c => c.Weight).MaximumLength(4);
+            RuleFor(c => c.Pb5k).MaximumLength(15);
+            RuleFor(c => c.Pb10k).MaximumLength(15);
+            RuleFor(c => c.PbHalfMarathon).MaximumLength(15);
+            RuleFor(c => c.PbFullMarathon).MaximumLength(15);
+            RuleFor(c => c.About).MaximumLength(25);
+            RuleFor(c => c.Accomplishments).MaximumLength(25);
+        }
+    }
+}
