53e197af7ebaac68056253270a63d6befa231fb7
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-08-02 20:18:23 +0200

    Added actions of Edit in RunnerProfileController

--- MyRun.MVC/Controllers/RunnerProfileController.cs
+++ MyRun.MVC/Controllers/RunnerProfileController.cs
@@ -1,6 +1,8 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using MyRun.Application.RunnerProfile.Commands.CreateRunnerProfile;
+using MyRun.Application.RunnerProfile.Commands.EditRunnerProfile;
 using MyRun.Application.RunnerProfile.Query.GetRunnerProfile;
 
 namespace MyRun.MVC.Controllers
@@ -8,10 +10,12 @@ namespace MyRun.MVC.Controllers
     public class RunnerProfileController : Controller
     {
         private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
 
-        public RunnerProfileController(IMediator mediator)
+        public RunnerProfileController(IMediator mediator, IMapper mapper)
         {
             _mediator = mediator;
+            _mapper = mapper;
         }
 
         //CREATE
@@ -39,5 +43,28 @@ namespace MyRun.MVC.Controllers
             return View(dto);
         }
         //GET PROFILE
+
+        //EDIT
+        [Route("RunnerProfile/{id}/Edit")]
+        public async Task<IActionResult> Edit (int id)
+        {
+            var dto = await _mediator.Send(new GetRunnerProfileQuery(id));
+
+            EditRunnerProfileCommand model = _mapper.Map<EditRunnerProfileCommand>(dto);
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("RunnerProfile/{id}/Edit")]
+        public async Task<IActionResult> Edit(int id, EditRunnerProfileCommand command)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(command);
+            }
+            await _mediator.Send(command);
+            return RedirectToAction(nameof(Index));
+        }
+        //EDIT
     }
 }
