4f02e4a249e6ad2d33ba4d3c703397953c88771b
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-08-09 20:28:37 +0200

    Created and implemented UserContext

---
+++ MyRun.Application/ApplicationUser/UserContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRun.Application.ApplicationUser
+{
+    public class UserContext
+    {
+        private readonly HttpContextAccessor _httpContextAccessor;
+
+        public UserContext(HttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public CurrentUser GetCurrentUser()
+        {
+            var user = _httpContextAccessor?.HttpContext?.User;
+            if(user == null)
+            {
+                throw new InvalidOperationException("Context user is not present");
+            }
+
+            var id = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
+            var email = user.FindFirst(c => c.Type == ClaimTypes.Email)!.Value;
+
+            return new CurrentUser(id, email);
+        }
+    }
+}
