5e5a514b4b47f79b517c42593d8055a93b760c1e
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-08-12 19:37:45 +0200

    Fixed UserContext.cs

--- MyRun.Application/ApplicationUser/UserContext.cs
+++ MyRun.Application/ApplicationUser/UserContext.cs
@@ -22,12 +22,17 @@ namespace MyRun.Application.ApplicationUser
             _httpContextAccessor = httpContextAccessor;
         }
 
-        public CurrentUser GetCurrentUser()
+        public CurrentUser? GetCurrentUser()
         {
             var user = _httpContextAccessor?.HttpContext?.User;
             if(user == null)
             {
                 throw new InvalidOperationException("Context user is not present");
+            }
+
+            if(user.Identity == null || !user.Identity.IsAuthenticated)
+            {
+                return null;
             }
 
             var id = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
