c9f5a2837593d632116864d1f229c8d71cfd0546
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-08-28 21:56:00 +0200

    Installed Moq

--- MyRun.ApplicationTests/ApplicationUser/UserContextTests.cs
+++ MyRun.ApplicationTests/ApplicationUser/UserContextTests.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Xunit;
+using MyRun.Application.ApplicationUser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyRun.ApplicationTests.ApplicationUser
+namespace MyRun.Application.ApplicationUser.Tests
 {
-    internal class UserContextTests
+    public class UserContextTests
     {
+        [Fact()]
+        public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
     }
 }
