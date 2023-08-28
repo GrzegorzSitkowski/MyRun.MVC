e74d2fe2480f81c4155d6d52b274a00bdd15210d
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-08-28 22:15:41 +0200

    Implemented UserContextTests.cs

--- MyRun.ApplicationTests/ApplicationUser/UserContextTests.cs
+++ MyRun.ApplicationTests/ApplicationUser/UserContextTests.cs
@@ -1,10 +1,8 @@
 ﻿using Xunit;
-using MyRun.Application.ApplicationUser;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Moq;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using FluentAssertions;
 
 namespace MyRun.Application.ApplicationUser.Tests
 {
@@ -13,7 +11,34 @@ namespace MyRun.Application.ApplicationUser.Tests
         [Fact()]
         public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
         {
-            var httpContextAccessor = new Mock
+            //arrange
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Email, "test@test.com")
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+            {
+                User = user
+            });
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+            //arrange
+
+            //act
+            var currentUser = userContext.GetCurrentUser();
+            //act
+
+            //arrange
+            currentUser.Should().NotBeNull();
+            currentUser!.Id.Should().Be("1");
+            currentUser.Email.Should().Be("test@test.com");
+            //arrange
         }
     }
 }
