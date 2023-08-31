a12f65e69d0dba9ed4e6fa07dd07a58d1f5315f3
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-08-31 20:12:24 +0200

    Created tests for EditWorkoutCommandValidator

---
+++ MyRun.ApplicationTests/Workout/Commands/EditWorkout/EditWorkoutCommandValidatorTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using MyRun.Application.Workout.Commands.EditWorkout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.TestHelper;
+
+namespace MyRun.Application.Workout.Commands.EditWorkout.Tests
+{
+    public class EditWorkoutCommandValidatorTests
+    {
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldNotHaveValidationError()
+        {
+            //average
+            var validator = new EditWorkoutCommandValidator();
+            var command = new EditWorkoutCommand()
+            {
+                Distance = "10",
+                Result = "00:47:42",
+                AveragePace = "04:51",
+                MaxPace = "04:10",
+                Calories = "410",
+                Note = "Good."
+            };
+            //average
+
+            //act
+            var result = validator.TestValidate(command);
+            //act
+
+            //assert
+            result.ShouldNotHaveAnyValidationErrors();
+            //assert
+        }
+
+        [Fact()]
+        public void Validate_WithInvalidCommand_ShoulHaveValidationErrors()
+        {
+            //average
+            var validator = new EditWorkoutCommandValidator();
+            var command = new EditWorkoutCommand()
+            {
+                Distance = "",
+                Result = "00:47:42000000000000000000",
+                AveragePace = "04:5100000000000000000",
+                MaxPace = "04:100000000000000000000",
+                Calories = "410",
+                Note = "Good."
+            };
+            //average
+
+            //act
+            var result = validator.TestValidate(command);
+            //act
+
+            //assert
+            result.ShouldHaveValidationErrorFor(c => c.Distance);
+            result.ShouldHaveValidationErrorFor(c => c.Result);
+            result.ShouldHaveValidationErrorFor(c => c.AveragePace);
+            result.ShouldHaveValidationErrorFor(c => c.MaxPace);
+            //assert
+        }
+    }
+}
